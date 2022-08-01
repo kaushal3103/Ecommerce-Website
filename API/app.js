be8ba2243e7740d7ee5cdd941102028c6f413338
kaushal3103 <81116597+kaushal3103@users.noreply.github.com>
2022-08-02 02:08:51 +0530

    Update app.js

--- API/app.js
+++ API/app.js
@@ -21,6 +21,14 @@ const path = require('path');
 app.use(express.json());
 
 app.use(cors());
+app.use(helmet());
+app.use(xss());
+
+app.use(rateLimitter({
+   windowMs: 15*60 *1000,
+   max : 100,
+}));
+
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/product',productRouter);
@@ -30,13 +38,6 @@ app.use("/api/v1/user",userRouter);
 app.use(errorHandlerMiddleware);
 app.use(notFound);
 
-/*
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
-*/
 
 const port = process.env.PORT || 3000;
 
